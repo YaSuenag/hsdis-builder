7a396f42b522a3cfa142dfdb3fdc64bd4f4dd0a6
Yasumasa Suenaga <yasuenag@gmail.com>
2023-04-01 21:17:40 +0900

    Refactoring build-hsdis.sh more safety

--- build-hsdis.sh
+++ build-hsdis.sh
@@ -7,7 +7,7 @@ cd builder
 
 if [ -z "$JDKVER" ]; then
   JDK_SRC_LINK=https://github.com/openjdk/jdk/archive/refs/heads/master.tar.gz
-  BOOT_JDK_VER=`curl -s https://api.adoptium.net/v3/info/available_releases | jq -r .most_recent_feature_release`
+  BOOT_JDK_VER=`curl -sSL https://api.adoptium.net/v3/info/available_releases | jq -r .most_recent_feature_release` || exit 1
 else
   JDK_SRC_LINK=https://github.com/openjdk/jdk/archive/refs/tags/$JDKVER.tar.gz
   JDK_RELEASE_VER=`echo $JDKVER | tr +- , | cut -d , -f 2`
@@ -19,7 +19,7 @@ else
 fi
 
 echo 'Download and extract JDK source'
-curl -sL $JDK_SRC_LINK | tar xvz
+curl -sSL $JDK_SRC_LINK | tar xvz || exit 2
 pushd jdk* > /dev/null
 JDK_SRC=`pwd`
 popd > /dev/null
@@ -34,8 +34,8 @@ echo "CPU architecture: $ARCH"
 echo
 
 echo 'Retrieve boot JDK'
-BOOT_JDK_LINK=`curl -s "https://api.adoptium.net/v3/assets/latest/$BOOT_JDK_VER/hotspot?architecture=$ARCH&image_type=jdk&os=linux&vendor=eclipse" | jq -r '.[0].binary.package.link'`
-curl -sL "$BOOT_JDK_LINK" | tar xvz
+BOOT_JDK_LINK=`curl -sSL "https://api.adoptium.net/v3/assets/latest/$BOOT_JDK_VER/hotspot?architecture=$ARCH&image_type=jdk&os=linux&vendor=eclipse" | jq -r '.[0].binary.package.link' || exit 3`
+curl -sL "$BOOT_JDK_LINK" | tar xvz || exit 4
 pushd jdk-$BOOT_JDK_VER** > /dev/null
 export JAVA_HOME=`pwd`
 popd > /dev/null
@@ -43,6 +43,6 @@ echo
 
 echo 'Build HSDIS'
 cd $JDK_SRC
-bash configure --with-hsdis=capstone
-make build-hsdis
-cp build/linux-*/support/hsdis/hsdis-*.so /out/
+bash configure --with-hsdis=capstone && \
+  make build-hsdis && \
+  cp build/linux-*/support/hsdis/hsdis-*.so /out/
